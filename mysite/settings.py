34dcbd823b9938cc476a4c94032ec8caf5d8effa
Никита Буравов <C:\Users\n.buravov\AppData\Roaming\The Bat!>
2026-04-12 01:56:36 +0400

    Yдаление аутентификации для пользователей, сайт полностью открыт

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,11 +119,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Login/Logout redirects
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/accounts/login/'
-LOGIN_URL = '/accounts/login/'
 
 
 # ============================================
--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
         path('admin/', admin.site.urls),
         path('', include('shparagalki.urls')),
-        path('accounts/', include('django.contrib.auth.urls')),
+      
     ]
